2caba5a41e5eb1ce30b47271e8327d731ab8796f
adam clark <adamclark44@gmail.com>
2020-06-04 21:43:48 -0500

    had marc fix all of the js for me

--- js/app.js
+++ js/app.js
@@ -4,21 +4,21 @@ console.log('This file is connected!');
 
 function guessingGame(){
 
-    var results;
+
     var favorite = prompt('What music do you love to listen to');
-    if (favorite = !null) {
-        results = 'Click the link to see if you favorite made the list';
-    } else {
-        results = 'PLEASE TYPE IN YOUR FAVORITE BAND OR MUSICIAN';
+    while(favorite == ''){
+        favorite = prompt('What music do you love to listen to');
     }
+    if (favorite == null){
+        favorite = 'who';
+    } else {
+        favorite = 'if '+ favorite;
+    }
+    var results = 'Click the link to see '+ favorite + ' made the list';
 
     return results;
 }
 var results = guessingGame();
 
 
-//console.log(`${results} ${pick} ${bestBand}`);
-
-console.log(results);
-
-document.write(results);
+document.getElementById('button').innerHTML = results;
